69cdef583f353c2efb99ca573e98da5b75ae8f15
milescaples <48931002+milescaples@users.noreply.github.com>
2019-04-04 09:47:41 +1100

    Add module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rigol',
+    version='0.1.0',
+    description='Python package to control Rigol devices.',
+    url='https://github.com/milescaples/PyRigol',
+    author='Miles Caples',
+    author_email='miles.caples@relectrify.com',
+    license='MIT',
+    packages=['rigol']
+)
