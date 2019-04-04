64e10cee484a942aa290e9165a62f42af3abf10a
milescaples <48931002+milescaples@users.noreply.github.com>
2019-04-04 12:53:34 +1100

    Update ds1000z.py
    Remove write to file and return raw_data instead

--- rigol/ds1000z.py
+++ rigol/ds1000z.py
@@ -31,6 +31,4 @@ class Ds1000z():
         self.visa_write(':WAV:MODE NORM')
         self.visa_write(':WAV:FORM ASC')
         raw_data = self.visa_ask_raw(':WAV:DATA?')[2+9:]
-        with open(filename, 'w') as f:
-            for item in raw_data:
-                f.write(str(item))
+        return raw_data
